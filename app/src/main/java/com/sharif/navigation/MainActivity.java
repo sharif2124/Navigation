f14d6b3b26e56950615b2898bb6497632f44682d
sharif2124 <azizur15-2124@diu.edu.bd>
2021-03-10 21:53:05 +0600

    complete

---
+++ app/src/main/java/com/sharif/navigation/MainActivity.java
@@ -0,0 +1,17 @@
+package com.sharif.navigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        androidx.appcompat.widget.Toolbar toolbar;
+        toolbar=findViewById(R.id.toolbarid);
+        setSupportActionBar(toolbar);
+    }
+}
